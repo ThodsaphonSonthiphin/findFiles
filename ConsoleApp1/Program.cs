b5f88e0a9cf197148a0aa93bc865f0c797fcc25e
thodsaphonsonthiphin <thodsaphonsp@gmail.com>
2019-09-19 21:18:41 +0700

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceDirectory = @"/Users/thodsaphonsonthiphin/Downloads";
+            
+
+            var docFile = Directory.EnumerateFiles(sourceDirectory, "*.docx", SearchOption.AllDirectories);
+            
+            foreach (string currentFile in docFile)
+            {
+                Console.WriteLine(currentFile);
+            }
+
+
+        }
+    }
+}
